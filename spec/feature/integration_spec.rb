98606219e5cb668ce720143aab3793d381189fe8
Ryan O'Malley <rd.omalley@comcast.net>
2022-09-15 22:47:33 -0500

    Added additional attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,12 +1,62 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
+RSpec.describe 'Creating a book with title', type: :feature do
     scenario 'valid inputs' do
         visit new_book_path
         fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'JK Rowling'
+        fill_in 'Price', with: '1.23'
+        select("2018", :from => 'book_published_1i')
+        select("September", :from => 'book_published_2i')
+        select("12", :from => 'book_published_3i')
         click_on 'Create Book'
         visit books_path
         expect(page).to have_content('harry potter')
+    end
+end
+
+RSpec.describe 'Creating a book with author', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'JK Rowling'
+        fill_in 'Price', with: '1.23'
+        select("2018", :from => 'book_published_1i')
+        select("September", :from => 'book_published_2i')
+        select("12", :from => 'book_published_3i')
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('JK Rowling')
+    end
+end
+
+RSpec.describe 'Creating a book with price', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'JK Rowling'
+        fill_in 'Price', with: '1.23'
+        select("2018", :from => 'book_published_1i')
+        select("September", :from => 'book_published_2i')
+        select("12", :from => 'book_published_3i')
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('1.23')
+    end
+end
+
+RSpec.describe 'Creating a book with published date', type: :feature do
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'JK Rowling'
+        fill_in 'Price', with: '1.23'
+        select("2018", :from => 'book_published_1i')
+        select("September", :from => 'book_published_2i')
+        select("12", :from => 'book_published_3i')
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('2018-09-12')
     end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
     subject do
-        described_class.new(title: 'harry potter')
+        described_class.new(title: 'harry potter',author: 'j.k. rowling',published: "2014/01/01",price: "1.23")
     end
 
     it 'is valid with valid attributes' do
@@ -12,6 +12,51 @@ RSpec.describe Book, type: :model do
 
     it 'is not valid without a name' do
         subject.title = nil
+        expect(subject).not_to be_valid
+    end
+end
+
+RSpec.describe Book, type: :model do
+    subject do
+        described_class.new(title: 'harry potter',author: 'j.k. rowling',published: "2014/01/01",price: "1.23")
+    end
+
+    it 'is valid with valid attributes' do
+        expect(subject).to be_valid
+    end
+
+    it 'is not valid without an author' do
+        subject.author = nil
+        expect(subject).not_to be_valid
+    end
+end
+
+RSpec.describe Book, type: :model do
+    subject do
+        described_class.new(title: 'harry potter',author: 'j.k. rowling',published: "2014/01/01",price: "1.23")
+    end
+
+    it 'is valid with valid attributes' do
+        expect(subject).to be_valid
+    end
+
+    it 'is not valid without an author' do
+        subject.published = nil
+        expect(subject).not_to be_valid
+    end
+end
+
+RSpec.describe Book, type: :model do
+    subject do
+        described_class.new(title: 'harry potter',author: 'j.k. rowling',published: "2014/01/01",price: "1.23")
+    end
+
+    it 'is valid with valid attributes' do
+        expect(subject).to be_valid
+    end
+
+    it 'is not valid without an author' do
+        subject.price = nil
         expect(subject).not_to be_valid
     end
 end
